443f3d53a28a8a628461ebe3d56aaad99106b20e
humans <humans@gmail.com>
2026-03-20 18:07:47 +0000

    feat: basic DelegatedPlaywright

--- hybrid-scraper/scrape.py
+++ hybrid-scraper/scrape.py
@@ -1,12 +1,18 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Playwright
 
 URL = "https://alifeofa.bearblog.dev/relaxing-is-not-a-waste-of-time/"
 
-def scrape_paragraphs(url):
-	with sync_playwright() as p:
-		browser = p.chromium.launch(headless=True)
-		page = browser.new_page()
-		page.goto(url)
+class DelegatedPlaywright(Playwright):
+    def __init__(self):
+        super().__init__()
+		with sync_playwright() as p:
+			self.playwright = p
+        self.browser = p.chromium.launch(headless=True)
+		self.page = browser.new_page()
+
+	def scrape_paragraphs(url):
+		self.page.goto(url)
+
 		paragraphs = page.query_selector_all("p")
 		for p_elem in paragraphs:
 			print(p_elem.inner_text())
