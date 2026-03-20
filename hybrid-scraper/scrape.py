920875d573ae2796d5994270c29b5416e0199fa6
humans <humans@gmail.com>
2026-03-20 18:10:04 +0000

    feat: added return to scrape_paragraphs

--- hybrid-scraper/scrape.py
+++ hybrid-scraper/scrape.py
@@ -10,13 +10,16 @@ class DelegatedPlaywright(Playwright):
         self.browser = p.chromium.launch(headless=True)
 		self.page = browser.new_page()
 
-	def scrape_paragraphs(url):
+	def scrape_paragraphs(url : string):
 		self.page.goto(url)
 
 		paragraphs = self.page.query_selector_all("p")
+		paragraphs_inner_text = []
 		for p_elem in paragraphs:
-			print(p_elem.inner_text())
+			paragraphs_inner_text.append(p_elem)
 		browser.close()
+		return paragraphs_inner_text
 
 if __name__ == "__main__":
-	scrape_paragraphs(URL)
+    playwright = DelegatedPlaywright()
+	playwright.scrape_paragraphs(URL)
