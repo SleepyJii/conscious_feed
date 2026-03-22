98752cfa73f6e6ddb1f699bde09b310ee4427221
humans <humans@gmail.com>
2026-03-22 00:28:41 +0000

    some config changes

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,4 +4,5 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR/.."
 
+. "$SCRIPT_DIR/load_config.sh"
 docker compose up "$@"
--- scripts/relaunch.sh
+++ scripts/relaunch.sh
@@ -4,6 +4,7 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR/.."
 
+. "$SCRIPT_DIR/load_config.sh"
 docker compose down
 docker compose -p fleet down
 "$SCRIPT_DIR/build.sh"
